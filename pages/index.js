c5ab8fc94c2e97cc687425f66da5f42f6e894a31
Jostein Eliassen <spam@jostein.be>
2019-08-16 13:55:54 +0200

    fix broken build

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,66 @@
 import { useContext } from "react";
 import Head from "next/head";
 import { client } from "../sanityClient";
-import { SiteConfigContext } from "../contexts";
+import { blurbTypes } from "../constants";
+import { SiteConfigContext, BlurbsContext } from "../contexts";
 import MainLayout from "../layouts/MainLayout";
 import Section from "../components/sections/Section";
 
 export default function Index(props) {
-  const { frontpage } = props;
+  const {
+    siteConfig,
+    siteConfig: { frontpage },
+    blurbs
+  } = props;
+
   const { title } = useContext(SiteConfigContext);
 
   if (!frontpage) return null;
 
   return (
-    <MainLayout>
-      <Head>
-        <title>{title}</title>
-      </Head>
+    <SiteConfigContext.Provider value={siteConfig}>
+      <BlurbsContext.Provider value={blurbs}>
+        <MainLayout>
+          <Head>
+            <title>{title}</title>
+          </Head>
 
-      <div>
-        {(frontpage.content || []).map(section => (
-          <Section key={section._key} section={section} />
-        ))}
-      </div>
-    </MainLayout>
+          <div>
+            {(frontpage.content || []).map(section => (
+              <Section key={section._key} section={section} />
+            ))}
+          </div>
+        </MainLayout>
+      </BlurbsContext.Provider>
+    </SiteConfigContext.Provider>
   );
 }
 
 Index.getInitialProps = async req => {
-  const result = await client.fetch(`
-      *[_id == "site-config"][0] { frontpage-> }
-  `);
+  const result = await client.fetch(
+    `{
+    "siteConfig": *[_id == "site-config"] [0] {
+      ...,
+      frontpage->,
+      logo {asset->{extension, url}},
+      mainNavigation[] -> {
+        "slug": slug.current,
+        "title": page->title
+      },
+      footerNavigation[] -> {
+        "slug": slug.current,
+        "title": page->title
+      }
+    },
+
+    "blurbs": *[_type in 
+      $blurbTypes] {
+        "imageFieldAsset": image.asset->,
+        ...
+      }
+  }`,
+    { blurbTypes }
+  );
 
   return {
     ...result
